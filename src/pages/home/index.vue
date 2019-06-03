e547d855f2f449aa6e6076656c953a870cc8b482
huahua_77 <690811017@qq.com>
2019-06-03 17:48:13 +0800

    swiper

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,47 @@
 <template>
     <div class="page">
-        项目库项目库项目库项目库
+        <slider :pages="pages" :sliderinit="sliderinit"></slider>
     </div>
 </template>
+<script>
+import slider from 'vue-concise-slider'// 引入slider组件
+export default {
+    data () {
+        return {
+            pages: [
+                {
+                    title: '图片1',
+                    style: {
+                        background: 'url(https://static.emeik.cn/FjZiPZ1ZvPMTpo4mLvlc5W9GGWVE)'
+                    }
+                },
+                {
+                    title: '图片2',
+                    style: {
+                        background: 'url(http://static.emeik.cn/ljmLvVbitOmeUZ1E1d19VRkwI59E)'
+                    }
+                },
+                {
+                    title: 'slide3',
+                    style: {
+                        background: '#4bbfc3'
+                    }
+                }
+            ],
+            sliderinit: {
+                currentPage: 0,
+                thresholdDistance: 500,
+                thresholdTime: 100,
+                autoplay: 1000,
+                loop: true,
+                direction: 'vertical',
+                infinite: 1,
+                slidesToScroll: 1
+            }
+        }
+    },
+    components: {
+        slider
+    }
+}
+</script>
--- src/pages/kada/index.vue
+++ src/pages/kada/index.vue
@@ -1,5 +1,34 @@
 <template>
     <div class="page">
-        卡达教育
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide"><img src="https://static.emeik.cn/FjZiPZ1ZvPMTpo4mLvlc5W9GGWVE"></div>
+                <div class="swiper-slide"><img src="https://static.emeik.cn/Fh2_GTX5NSE6SC0_DmF74eGwYIAK"></div>
+                <div class="swiper-slide"><img src="https://static.emeik.cn/FiCYaej0zRkLVcwiScN6mGwohoe1"></div>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
     </div>
 </template>
+<script>
+export default{
+    data () {
+        return {
+            imgList: [
+                {
+                    title: '1',
+                    src: 'https://static.emeik.cn/FjZiPZ1ZvPMTpo4mLvlc5W9GGWVE'
+                },
+                {
+                    title: '2',
+                    src: 'https://static.emeik.cn/Fh2_GTX5NSE6SC0_DmF74eGwYIAK'
+                },
+                {
+                    title: '3',
+                    src: 'https://static.emeik.cn/FiCYaej0zRkLVcwiScN6mGwohoe1'
+                }
+            ]
+        }
+    }
+}
+</script>
