7208d99ce20b5ee125255e8bf22cfd850f0d56db
huahua_77 <690811017@qq.com>
2019-06-17 14:23:27 +0800

    me

--- src/pages/me/index.vue
+++ src/pages/me/index.vue
@@ -1,18 +1,19 @@
 <style lang="scss" src="../../assets/css/me.scss"></style>
-import func from './vue-temp/vue-editor-bridge';
 <template>
     <div class="page">
         <div class="box">
             <div>头部数量<span>{{itemsL.length}}</span></div>
             <ul>
                 <li v-for="(item, index) in itemsL" :key="index">
-                    <input type="checkbox" v-model="item.checkstatus">{{item.name}}
+                    <input type="checkbox" v-model="item.checked">{{item.name}}
                 </li>
             </ul>
         </div>
         <div class="box-btn">
-            <span @click="upfun()">移上去的按钮</span>
-            <span @click="downfun()">移下去的按钮</span>
+            <span @click="upselectallornot()">头部全选/不选</span>
+            <span @click="upfun()">移上</span>
+            <span @click="downfun()">移下</span>
+            <span>底部全选/不选</span>
         </div>
         <div class="box">
             <div>底部数量<span>{{itemsR.length}}</span></div>
@@ -48,6 +49,14 @@ export default {
         }
     },
     methods: {
+        upselectallornot: function () {
+            if (this.itemsL.length > 0) {
+                let ss = this.itemsL.filter(function (item) {
+                    return item.checked === true;
+                })
+                console.log(ss.length)
+            }
+        },
         upfun: function () {
             this.arrR = this.itemsR.filter(function (item) {
                 return item.checked === true;
